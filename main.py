7ebc284129b9d2f88170fa46016494a137293e66
b.nurseitov <bulanurseitov@gmail.ru>
2023-12-03 15:06:22 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+questionery = 'Здравствуйте, вы проходите анкету'
+print(questionery)
+name = input('Введите ваше имя: ')
+surname = input('Введите вашу фамилию: ')
+date_birth = input('Введите ваш год рождения: ')
+course = input('Нравится ли вам курс? ')
+question = input('Что вы ожидаете в дальнейших занятиях? ')
+full_name = name + ' ' + surname
+print("Введите ваше ФИО:", full_name)
+print("Введите ваш год рождения:", date_birth)
+print("Нравится ли вам курс?", course)
+print("Что вы ожидаете в дальнейших занятиях?", question)
+
+
